8915a0763d75963b2184b21348efde97fa448ba2
Akbar Rizqullah Putra Susanto <akbarrizqullah228@gmail.com>
2024-03-18 16:04:53 +0700

    Initial Commit Main

--- lib/core/extensions/int_ext.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension IntegerExt on int {
-  String get currencyFormatRp => NumberFormat.currency(
-        locale: 'id',
-        symbol: 'Rp. ',
-        decimalDigits: 0,
-      ).format(this);
-}
--- lib/core/extensions/string_ext.dart
+++
@@ -1,7 +0,0 @@
-extension StringExt on String {
-  int get toIntegerFromText {
-    final cleanedText = replaceAll(RegExp(r'[^0-9]'), '');
-    final parsedValue = int.tryParse(cleanedText) ?? 0;
-    return parsedValue;
-  }
-}
